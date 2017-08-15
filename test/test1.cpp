226fbded1ffe9c1534a0afc16b8b1cac8e2f3db9
dixtel <dixtel2001@gmail.com>
2017-08-15 21:57:41 +0200

    disable test

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,35 +1,35 @@
-#include "include/rectangle.h"
-#include "include/render.h"
-#include "include/window.h"
+//#include "include/rectangle.h"
+//#include "include/render.h"
+//#include "include/window.h"
 
-#include <SDL2/SDL.h>
+//#include <SDL2/SDL.h>
 
-int main(int argc, char *argv[])
-{
+//int main(int argc, char *argv[])
+//{
 
-    Window window;
-    if (!window.Init("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)) {
+//    Window window;
+//    if (!window.Init("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)) {
 
-        return 1;
-    }
+//        return 1;
+//    }
 
-    Render render(window.GetRenderer(), SDL_Color{0, 0, 0, 255});
+//    Render render(window.GetRenderer(), SDL_Color{0, 0, 0, 255});
 
-    Rectangle object;
+//    Rectangle object;
 
-    object.SetSize({100, 50});
-    object.SetPosition({0, 0});
-    object.SetRectColor({0, 255, 0, 255});
-
-
-    object.SetPosition({200, 200});
+//    object.SetSize({100, 50});
+//    object.SetPosition({0, 0});
+//    object.SetRectColor({0, 255, 0, 255});
 
 
-    render.Clear();
-    render.Draw(&object);
-    render.Display();
+//    object.SetPosition({200, 200});
 
-    SDL_Delay(3 * 1000);
 
-    return 0;
-}
+//    render.Clear();
+//    render.Draw(&object);
+//    render.Display();
+
+//    SDL_Delay(3 * 1000);
+
+//    return 0;
+//}

830dcc6a209600617921e87f53b6f049d2dd49b6
dixtel <dixtel2001@gmail.com>
2017-08-15 21:57:27 +0200

    init main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include <SDL2/SDL.h>
 
-//int main(int argc, char* argv[]) {
+#include "include/gameengine.h"
 
+int main(int argc, char* argv[]) {
 
-//}
+    GameEngine game_engine;
+
+    if (!game_engine.Init()) {
+
+        return 1;
+    }
+
+    game_engine.Start();
+    game_engine.CleanUp();
+
+    return 0;
+}

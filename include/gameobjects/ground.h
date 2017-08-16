a0f1bafe533f7643b03e06077705d884578e85e9
dixtel <dixtel2001@gmail.com>
2017-08-16 15:20:34 +0200

    create gameobject ground

---
+++ include/gameobjects/ground.h
@@ -0,0 +1,37 @@
+#ifndef GROUND_H
+#define GROUND_H
+
+#include <SDL2/SDL.h>
+
+#include "include/rectangle.h"
+#include "include/rectanglecollision.h"
+#include "include/gameobject.h"
+
+#include "include/vectro2.h"
+
+class Ground : public GameObject
+{
+public:
+
+    Ground();
+
+    void Init(Vector2f position, Vector2u size, SDL_Texture *texture);
+
+    void UpdatePhysics(float dt);
+    void Update();
+    void HandleInput(GameObjectInput::Type input);
+
+    Rectangle *GetRectangle();
+    RectangleCollision *GetRectangleCollision();
+private:
+
+    SDL_Rect destination_texture;
+
+    Rectangle rectangle;
+    RectangleCollision rectangle_collision;
+
+    Vector2f position;
+    Vector2u size;
+};
+
+#endif // GROUND_H
---
+++ src/gameobjects/ground.cpp
@@ -0,0 +1,48 @@
+#include "include/gameobjects/ground.h"
+
+Ground::Ground() {
+
+}
+
+void Ground::Init(Vector2f position, Vector2u size, SDL_Texture *texture) {
+
+    this->position = position;
+    this->size = size;
+
+    destination_texture = {0, 0, size.x, size.y};
+
+    rectangle.SetPosition(position);
+    rectangle.SetSize(size);
+    rectangle.SetTexture(texture);
+    rectangle.SetSourceTexture(destination_texture);
+
+    rectangle_collision.Init(position, size, CollisionType::STATICBODY);
+}
+
+void Ground::UpdatePhysics(float dt) {
+
+    rectangle_collision.SetPosition(position);
+}
+
+void Ground::Ground::Update() {
+
+    position = rectangle_collision.GetPosition();
+
+    rectangle.SetPosition(position);
+    rectangle.SetSize(size);
+    rectangle.SetSourceTexture(destination_texture);
+}
+
+void Ground::HandleInput(GameObjectInput::Type input) {
+
+}
+
+Rectangle *Ground::GetRectangle() {
+
+    return &rectangle;
+}
+
+RectangleCollision *Ground::GetRectangleCollision() {
+
+    return &rectangle_collision;
+}

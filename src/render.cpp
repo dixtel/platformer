689ed7f9d3260da85f91aa7821833305e4ceaa94
dixtel <dixtel2001@gmail.com>
2017-08-16 14:08:47 +0200

    refactor

--- src/render.cpp
+++ src/render.cpp
@@ -20,13 +20,20 @@ void Render::Clear() {
 
 void Render::Draw(Rectangle *rectangle) {
 
+    if (!rectangle) {
+
+        SDL_Log("Error: rectangle to draw are NULL (Render)");
+        return;
+    }
+
     SDL_SetRenderDrawColor(renderer, rectangle->GetRectColor().r, rectangle->GetRectColor().g, rectangle->GetRectColor().b, rectangle->GetRectColor().a);
-    SDL_RenderFillRect(renderer, &rectangle->GetRect());
+    SDL_RenderFillRect(renderer, &rectangle->GetRectDestination());
     SDL_SetRenderDrawColor(renderer, clear_color.r, clear_color.g, clear_color.b, clear_color.a);
-    SDL_RenderCopy(renderer,
-                   rectangle->GetTexture(),
-                   &rectangle->GetRect(),
-                   &rectangle->GetDestinationTexture());
+//    SDL_RenderCopy(renderer,
+//                   rectangle->GetTexture(),
+//                   &rectangle->GetSourceTexture(),
+//                   &rectangle->GetRectDestination());
+
 }
 
 void Render::Display() {

6e102156d038fc08ecbe26aeb9d90a1a79d1a15f
dixtel <dixtel2001@gmail.com>
2017-08-16 14:08:32 +0200

    change way to load texture

--- src/imageloader.cpp
+++ src/imageloader.cpp
@@ -11,19 +11,10 @@ void ImageLoader::Init(Render *render) {
 
 SDL_Texture *ImageLoader::LoadImage(char *path) {
 
-    SDL_Surface *surface = IMG_Load(path);
-
-    if (surface == nullptr) {
-
-        SDL_Log("Error: cannot load image: %s %s (ImageLoader)", path, SDL_GetError());
-        return nullptr;
-    }
-
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(render->GetRenderer(), surface);
+    SDL_Texture *texture = IMG_LoadTexture(render->GetRenderer(), path);
 
     if (texture == nullptr) {
 
-        SDL_FreeSurface(surface);
         SDL_Log("Error: cannot create texture from surface: %s %s (ImageLoader)", path, SDL_GetError());
         return nullptr;
     }

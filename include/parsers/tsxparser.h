5dc2cfb6324f4a96c2b09a719bd7f0864e319ed8
dixtel <dixtel2001@gmail.com>
2017-08-30 13:07:54 +0200

    Create .tmx parser class

---
+++ include/parsers/tsxparser.h
@@ -0,0 +1,44 @@
+#ifndef TSXPARSER_H
+#define TSXPARSER_H
+
+#include <SDL2/SDL_log.h>
+
+#include <rapidxml.hpp>
+#include <rapidxml_utils.hpp>
+#include <stdexcept>
+#include <sstream>
+
+class TSXParser
+{
+public:
+
+    TSXParser();
+    ~TSXParser();
+
+    bool LoadTSX(std::string path);
+
+    unsigned GetImageWidth();
+    unsigned GetImageHeight();
+    unsigned GetTileWidth();
+    unsigned GetTileHeight();
+    unsigned GetTileCount();
+    unsigned GetColumns();
+    std::string GetPathImage();
+private:
+
+    template <typename T>
+    unsigned ConvertToUnsigned(T num);
+
+    unsigned image_width;
+    unsigned image_height;
+    unsigned tile_width;
+    unsigned tile_height;
+    unsigned tile_counts;
+    unsigned tile_columns;
+    std::string path_image;
+
+    rapidxml::file<> *file = nullptr;
+    rapidxml::xml_document<> documnet;
+};
+
+#endif // TSXPARSER_H
---
+++ src/parsers/tsxparser.cpp
@@ -0,0 +1,103 @@
+#include "include/parsers/tsxparser.h"
+
+TSXParser::TSXParser() {
+
+    image_width = 0;
+    image_height = 0;
+    tile_width = 0;
+    tile_height = 0;
+    tile_counts = 0;
+    tile_columns = 0;
+}
+
+TSXParser::~TSXParser() {
+
+    if (file) {
+
+        delete file;
+        file = nullptr;
+    }
+}
+
+
+bool TSXParser::LoadTSX(std::string path) {
+
+    if (file) {
+
+        delete file;
+        file = nullptr;
+    }
+
+    try {
+        file = new rapidxml::file<>(path.c_str());
+    } catch (std::runtime_error &e) {
+
+        SDL_Log("(TSXPARSER) Error load .tsx file %s", e.what());
+        return false;
+    }
+
+    documnet.parse<0>(file->data());
+
+    rapidxml::xml_node<> *tileset_node = documnet.first_node("tileset");
+
+    tile_width = ConvertToUnsigned(tileset_node->first_attribute("tilewidth")->value());
+    tile_height = ConvertToUnsigned(tileset_node->first_attribute("tileheight")->value());
+    tile_counts = ConvertToUnsigned(tileset_node->first_attribute("tilecount")->value());
+    tile_columns = ConvertToUnsigned(tileset_node->first_attribute("columns")->value());
+
+    rapidxml::xml_node<> *image_node = tileset_node->first_node("image");
+
+    path_image = ConvertToUnsigned(image_node->first_attribute("source")->value());
+    image_width = ConvertToUnsigned(image_node->first_attribute("width")->value());
+    image_height = ConvertToUnsigned(image_node->first_attribute("height")->value());
+
+    // TODO parse each properties object
+
+    return true;
+}
+
+unsigned TSXParser::GetImageWidth() {
+
+    return image_width;
+}
+
+unsigned TSXParser::GetImageHeight() {
+
+    return image_height;
+}
+
+unsigned TSXParser::GetTileWidth() {
+
+    return tile_width;
+}
+
+unsigned TSXParser::GetTileHeight() {
+
+    return tile_height;
+}
+
+unsigned TSXParser::GetTileCount() {
+
+    return tile_counts;
+}
+
+unsigned TSXParser::GetColumns() {
+
+    return tile_columns;
+}
+
+std::string TSXParser::GetPathImage() {
+
+    return path_image;
+}
+
+template <typename T>
+unsigned TSXParser::ConvertToUnsigned(T num) {
+
+    unsigned re_val;
+    std::stringstream ss;
+    ss << num;
+    ss >> re_val;
+
+    return re_val;
+}

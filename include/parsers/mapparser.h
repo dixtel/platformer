fb061f94cf0a61af054065f11d80d27d3fd01d54
dixtel <dixtel2001@gmail.com>
2017-09-01 15:20:39 +0200

    add map parser

--- include/parsers/mapparser.h
+++ include/parsers/mapparser.h
@@ -1,11 +1,74 @@
 #ifndef MAPPARSER_H
 #define MAPPARSER_H
 
+#include <SDL2/SDL_rect.h>
+
+#include "include/parsers/tmxparser.h"
+#include "include/parsers/tsxparser.h"
+
+#include <vector>
+
+enum TileType {
+    TileType_NONE = 0,
+    TileType_GROUND = 1,
+    TileType_DECORATION_BOX1 = 2,
+    TileType_PLAYER = 3,
+    TileType_DECORATION_BOX2 = 4,
+    TileType_ENDMAP = 5,
+};
+
+enum LayerType {
+    LayerType_OTHER,
+    LayerType_TOP,
+    LayerType_MIDDLE,
+    LayerType_DOWN,
+};
+
+static std::string TileTypeToString(TileType type) {
+
+     if (type == TileType_GROUND)
+         return "Ground";
+     else if (type == TileType_DECORATION_BOX1)
+         return "Decoration_box1";
+     else if (type == TileType_PLAYER)
+         return "Player";
+     else if (type == TileType_DECORATION_BOX2)
+         return "Decoration_box2";
+     else if (type == TileType_ENDMAP)
+         return "End map";
+}
+
+static std::string LayerTypeToString(LayerType type) {
+
+     if (type == LayerType_OTHER)
+         return "Other";
+     else if (type == LayerType_TOP)
+         return "Top";
+     else if (type == LayerType_MIDDLE)
+         return "Middle";
+     else if (type == LayerType_DOWN)
+         return "Down";
+}
 
 class MapParser
 {
 public:
+
     MapParser();
+
+    bool LoadMap(std::string path_map, std::string path_tileset);
+
+    unsigned GetTileWidth();
+    unsigned GetTileHeight();
+    unsigned GetMapWidth();       // height in tiles
+    unsigned GetMapHeight();      // width in tiles
+    std::string GetImagePath();
+    std::vector<TileType> GetMapLayer(LayerType name_layer);
+    SDL_Rect GetImageDestination(TileType tile);
+private:
+
+    TMXParser tmx_parser;
+    TSXParser tsx_parser;
 };
 
 #endif // MAPPARSER_H
---
+++ src/parsers/mapparser.cpp
@@ -0,0 +1,70 @@
+#include "include/parsers/mapparser.h"
+
+MapParser::MapParser()
+{
+
+}
+bool MapParser::LoadMap(std::string path_map, std::string path_tilese) {
+
+    if (!tmx_parser.LoadTMX(path_map) || !tsx_parser.LoadTSX(path_tilese))
+        return false;
+
+    return true;
+}
+
+unsigned MapParser::GetTileWidth() {
+
+    return tsx_parser.GetTileWidth();
+}
+
+unsigned MapParser::GetTileHeight() {
+
+    return tsx_parser.GetTileHeight();
+}
+
+unsigned MapParser::GetMapWidth() {
+
+    return tmx_parser.GetWidth();
+}
+
+unsigned MapParser::GetMapHeight() {
+
+    return tmx_parser.GetHeight();
+}
+
+std::string MapParser::GetImagePath() {
+
+    return tsx_parser.GetPathImage();
+}
+
+std::vector<TileType> MapParser::GetMapLayer(LayerType layer_type) {
+
+    std::vector<int> raw_layer = tmx_parser.GetLayer(LayerTypeToString(layer_type));
+    std::vector<TileType> layer;
+
+    for (int i = 0; i < raw_layer.size(); ++i) {
+
+        if (raw_layer[i] == TileType_GROUND)
+            layer.push_back(TileType_GROUND);
+        else if (raw_layer[i] == TileType_DECORATION_BOX1)
+            layer.push_back(TileType_DECORATION_BOX1);
+        else if (raw_layer[i] == TileType_PLAYER)
+            layer.push_back(TileType_PLAYER);
+        else if (raw_layer[i] == TileType_DECORATION_BOX2)
+            layer.push_back(TileType_DECORATION_BOX2);
+        else if (raw_layer[i] == TileType_ENDMAP)
+            layer.push_back(TileType_ENDMAP);
+        else
+            layer.push_back(TileType_NONE);
+    }
+
+    return layer;
+}
+
+SDL_Rect MapParser::GetImageDestination(TileType tile) {
+
+    unsigned x_pos = (tile - 1)  % tsx_parser.GetColumns();
+    unsigned y_pos = (tile - 1) / tsx_parser.GetColumns();
+
+    return SDL_Rect {x_pos * GetTileWidth(), y_pos * GetTileHeight(), GetTileWidth(), GetTileHeight()};
+}
